458cd3fd437b1e9a269fd1a82d111ce195ed7c6e
Thibaut Metivet <thibaut.metivet@cea.fr>
2014-07-11 17:57:49 +0200

    remove template on minimizers options

--- include/Minimizer.hpp
+++ include/Minimizer.hpp
@@ -11,67 +11,71 @@
  #include "Globals.hpp"
  #include "Function.hpp"
 
- namespace LQCDA {
- 	namespace MIN {
+BEGIN_NAMESPACE(LQCDA)
+BEGIN_NAMESPACE(MIN)
 
- 		template<template<typename> class MINTYPE>
- 		class Options;
- 		template<typename T>
- 		class Minimizer;
+class MinimizerOptions
+{
+public:
+	Verbosity verbosity;
 
- 		template<typename T>
- 		using DEFAULT = Minimizer<T>;
-
- 		template<typename T>
- 		class Minimizer
- 		{
- 		public:
- 			struct Result
- 			{
- 				std::vector<double> minimum;
- 				std::vector<double> errors;
- 				double final_cost;
- 				bool is_valid;
- 			};
-
- 			// Typedefs
- 			typedef T scalar_type;
-
- 		public:
- 			// Destructor
- 			virtual ~Minimizer() noexcept = default;
-
- 			// Options
- 			virtual Options<DEFAULT>& options() =0;
-
- 			// Minimize
- 			virtual Result minimize(
- 				const ScalarFunction<scalar_type>& F, 
- 				const std::vector<scalar_type>& x0) =0;
- 		};
-
-
- 		template<>
- 		class Options<DEFAULT>
- 		{
- 		public:
- 			Verbosity verbosity;
-
- 		public:
- 			Options() {
- 				verbosity = NORMAL;
- 			}
-
- 			virtual ~Options() noexcept = default;
- 		};
-
- 		template<typename T>
- 		std::ostream& operator<< (std::ostream& out, const typename Minimizer<T>::Result& result)
- 		{
- 			out << "Final cost = " << result.final_cost << std::endl;
- 			return out;
- 		}
+public:
+	MinimizerOptions() {
+		verbosity = NORMAL;
 	}
- }
+
+	virtual ~MinimizerOptions() noexcept = default;
+
+	virtual void print(std::ostream& os) const
+	{
+		os << "Minimizer options:\n"
+		<< "\tverbosity = " << verbosity << std::endl;
+	}
+};
+
+inline std::ostream& operator<<(std::ostream& os, const MinimizerOptions& opts)
+{
+	opts.print(os);
+	return os;
+}
+
+template<typename T>
+class Minimizer
+{
+public:
+	struct Result
+	{
+		std::vector<double> minimum;
+		std::vector<double> errors;
+		double final_cost;
+		bool is_valid;
+	};
+
+	// Typedefs
+	typedef MinimizerOptions OptionsType;
+	typedef T scalar_type;
+
+public:
+	// Destructor
+	virtual ~Minimizer() noexcept = default;
+
+	// Options
+	virtual OptionsType& options() =0;
+
+	// Minimize
+	virtual Result minimize(
+		const ScalarFunction<scalar_type>& F, 
+		const std::vector<scalar_type>& x0) =0;
+};
+
+template<typename T>
+std::ostream& operator<< (std::ostream& out, const typename Minimizer<T>::Result& result)
+{
+	out << "Final cost = " << result.final_cost << std::endl;
+	return out;
+}
+
+END_NAMESPACE
+END_NAMESPACE
 
 #endif // MINIMIZER_HPP
